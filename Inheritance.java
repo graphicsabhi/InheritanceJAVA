39f7211930bacc21db901de8cd1cc118585a4376
Satyam <satyamsobhraj819@gmail.com>
2022-08-24 12:35:30 +0530

    Inheritance JAVA

---
+++ Inheritance.java
@@ -0,0 +1,14 @@
+class shape {
+    String color;
+}
+
+class Triangle extends shape {
+
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Triangle T1 = new Triangle();
+        T1.color = "red";
+    }
+}
